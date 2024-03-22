2123c8b7e7e96a01832e835418f4d9fc454db22b
Oleh Hurmanchuk <gyrmanchyk230506@gmail.com>
2024-03-22 14:53:28 +0200

    add article cards

---
+++ src/components/ArticlesList.vue
@@ -0,0 +1,48 @@
+<script setup>
+const props = defineProps(['articles']);
+
+</script>
+
+<template>
+  <ul class="articles_list">
+    <li v-for="article in props.articles" class="article_card_container">
+      <v-card
+        v-if="article.title !== '[Removed]'"
+        :title="article.title"
+        :text="article.description"
+        :href="article.url"
+        class="article_card"
+        color="light-blue"
+      ></v-card>
+      <v-card
+        v-else
+        title="This article was removed"
+        text="This article was removed, so there is nothing to show"
+        class="article_card"
+        color="pink"
+        variant="tonal"
+      ></v-card>
+    </li>
+  </ul>
+</template>
+
+<style>
+.articles_list {
+  display: flex;
+  align-items: center;
+  /* justify-content: center; */
+  flex-direction: column;
+  gap: 0.5rem;
+}
+
+.article_card_container {
+  width: 35rem;
+  list-style: none;
+}
+
+.article_card {
+  padding: 1rem;
+}
+
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,28 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useFetch } from '@vueuse/core'
+import ArticlesList from '../components/ArticlesList.vue'
 const NEWS_API_KEY = import.meta.env.VITE_NEWS_API_KEY;
 
 let news = ref([]);
 
 onMounted(async () => {
-  const { data, isFetching, error } = await useFetch(`https://newsapi.org/v2/everything?q=Apple&apiKey=${NEWS_API_KEY}`).json()
+  const { data, isFetching, error } = await useFetch('https://newsapi.org/v2/everything?q=Apple', {
+    async beforeFetch({ options }) {
+      options.headers = {
+        ...options.headers,
+        "X-Api-Key": NEWS_API_KEY,
+      }
+    }
+  }).json()
 
   news.value = data.value.articles;
 })
 </script>
 
 <template>
-    <main>
-        <h1>Home page</h1>
-        <ul>
-          <h2>{{ news.length }}</h2>
-          <li v-for="article in news">
-            {{ article.title }}
-          </li>
-        </ul>
-    </main>
+  <main>
+    <h1>Home page</h1>
+    <ArticlesList :articles='news' />
+  </main>
 </template>

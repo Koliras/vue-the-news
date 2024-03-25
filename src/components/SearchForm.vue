4d993c6428a4e0f5c18987f49b5bc98590360d42
Oleh Hurmanchuk <gyrmanchyk230506@gmail.com>
2024-03-25 14:31:26 +0200

    add date to picker

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive, computed } from 'vue'
+import { reactive, computed, ref } from 'vue'
 import { useFetch } from '@vueuse/core'
 import { useVuelidate } from '@vuelidate/core'
 import { required, minLength } from '@vuelidate/validators'
@@ -7,12 +7,19 @@ import { SEARCH_IN_FIELDS, initialNewsUrl, SORT_METHODS } from '../utils/constan
 
 const NEWS_API_KEY = import.meta.env.VITE_NEWS_API_KEY
 const emit = defineEmits([ 'search-submit' ]);
+const showDateToPicker = ref(false);
 
 const formState = reactive({
   query: '',
   searchIn: [],
   sortBy: 'publishedAt',
+  dateTo: undefined,
+  dateFrom: undefined,
 })
+
+const dateToBtn = ref(null)
+const dateToFormated = computed(() => formState.dateTo?.toISOString().slice(0, 10));
+const dateFromFormated = computed(() => formState.dateFrom?.toISOString().slice(0, 10));
 
 const rules = computed(() => {
   return {
@@ -22,6 +29,8 @@ const rules = computed(() => {
     },
     searchIn: {},
     sortBy: {},
+    dateTo: {},
+    dateFrom: {},
   }
 })
 
@@ -30,9 +39,14 @@ const v$ = useVuelidate(rules, formState);
 async function handleSubmit() {
   const passed = await v$.value.$validate();
   if (!passed) return;
+
   let urlString = initialNewsUrl + `&q=${formState.query}&sortBy=${formState.sortBy}`;
   if (formState.searchIn.length) {
     urlString += `&searchIn=${formState.searchIn.join(',')}`
+  }
+
+  if (formState.dateTo) {
+    urlString += `&to${formState.dateTo}`
   }
   const uriEncodedString = encodeURI(urlString);
 
@@ -84,6 +98,31 @@ async function handleSubmit() {
       </v-radio-group>
     </v-container>
 
+    <div class="datePickerContainer">
+      <button
+        type="button"
+        @click="showDateToPicker = !showDateToPicker"
+        ref="dateToBtn"
+      >
+        To: {{ dateToFormated || "" }}
+      </button>
+
+      <span
+        v-if="showDateToPicker"
+        class="datePickerBack"
+        @click="showDateToPicker = false"
+      ></span>
+
+      <v-date-picker
+        v-if="showDateToPicker"
+        class="datePicker"
+        id="dateTo"
+        @update:modelValue="() => showDateToPicker = false"
+        v-model="formState.dateTo"
+        hide-header
+      ></v-date-picker>
+    </div>
+
     <v-btn type="submit">Search</v-btn>
   </form>
 </template>
@@ -115,5 +154,22 @@ async function handleSubmit() {
 .sortContainer {
   padding: 0;
   margin-top: 2rem;
+}
+
+.datePickerContainer {
+  position: relative;
+}
+
+.datePicker {
+  position: absolute;
+  z-index: 2;
+}
+
+.datePickerBack {
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
 }
 </style>
